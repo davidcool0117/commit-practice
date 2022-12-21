cb697fec2ab2f1bbdd2d902aa85968ebb3c13052
davidcool0117 <davidcool0117@gmail.com>
2022-12-21 14:39:51 +0900

    javascript add

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+	document.querySelector("#layer button").addEventListener("click", function () {
+		document.querySelector("#layer").style.display = "none";
+	});
+});
+
+$
